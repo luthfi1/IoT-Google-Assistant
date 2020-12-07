61633e1496ad53722f0a805bfa9cb49958d00a54
luthfi1 <68719137+luthfi1@users.noreply.github.com>
2020-12-07 19:56:28 +0700

    Add files via upload

---
+++ led_google_assistant.ino
@@ -0,0 +1,57 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WIFI.h>
+#include <BlynkSimpleEsp8266.h>
+char auth[]="aio_lIny31po6AlM7gSGYFWZb77hmPoo"
+char  ssid[] ="Lf ds"
+char  pass[] ="12345678#"
+ void setup ()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  
+}
+
+void loop()
+{
+  Blynk.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ [
+
+
+
+
+
+'
+"
+
+
+
+
+
+
+
+
+
+'
